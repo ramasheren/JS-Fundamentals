a4df5006b1e347bf7bc82cb2cfa32da2765c9307
Rama <ramasheren@users.noreply.github.com>
2025-05-31 06:05:15 +0300

    2

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+for(let i = 0; 1<3; 1++){
+    
+    switch(i) {
+    case 0:
+        console.log("C is fun");
+        break;
+    case 1:
+        console.log("Python is cool");
+        break;
+    case 2:
+        console.log("JavaScript is amazing");
+        break;
+    default:
+        break;
+    }
+}
+   
