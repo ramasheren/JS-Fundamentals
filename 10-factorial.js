1e386926e1a6ed8dcfe5d14cc5eb37f4d083462d
Rama <ramasheren@users.noreply.github.com>
2025-05-31 06:58:54 +0300

    2

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,16 +1,19 @@
 
-const arg = parseInt(process.argv[2]);
 
-if (isNaN(arg)) {
-  console.log(1);
-} else {
-  console.log(factorial(arg));
-}
+const arg = process.argv[2];
 
-function factorial(n) {
-  let result = 1;
-  for (let i = n; i > 1; i--) {
+function factorialBigInt(n) {
+  let result = 1n;
+  for (let i = BigInt(n); i > 1n; i--) {
     result *= i;
   }
   return result;
+}
+
+if (!arg || isNaN(parseInt(arg))) {
+  console.log("Missing size");
+} else {
+  const n = parseInt(arg);
+  const fact = factorialBigInt(n);
+  console.log(fact.toString());
 }
