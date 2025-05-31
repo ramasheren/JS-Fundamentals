162f000b9a0e46bb5187074cbbf3260b444a53a5
Rama <ramasheren@users.noreply.github.com>
2025-05-31 06:09:09 +0300

    2

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,17 +1,7 @@
+let languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
 for(let i = 0; 1<3; 1++){
     
-    switch(i) {
-    case 0:
-        console.log("C is fun");
-        break;
-    case 1:
-        console.log("Python is cool");
-        break;
-    case 2:
-        console.log("JavaScript is amazing");
-        break;
-    default:
-        break;
-    }
+        console.log(languages[i]);
+
 }
-   
